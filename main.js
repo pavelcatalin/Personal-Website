75c243ea8b3b1a3e01d899c003bae8eab8dba0f4
Pavel <pavelcc11@yahoo.com>
2021-03-18 02:06:36 +0200

    update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", function(){
   var scrollInfo= window.pageYOffset;
   var moveLeft = document.querySelectorAll(".moveLeft")[0];
   moveLeft.style.transform = `translateX(-${scrollInfo *0.025}%)`;
+  
   var moveLeft1 = document.querySelectorAll(".moveLeft")[1];
   moveLeft1.style.transform = `translateX(-${scrollInfo *0.005}%)`;
   var moveRight = document.querySelectorAll(".moveRight")[0];
